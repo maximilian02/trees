0bc81bff37c2daf0c720babb9e5e9a3dcd2da880
Max Zelarayan <max.z_@hotmail.com>
2023-01-11 21:49:07 -0300

    Adding binary tree insert and search operations

--- binary.js
+++ binary.js
@@ -1,8 +1,84 @@
+import { VALUE_PRESENT, VALUE_NOT_PRESENT } from './utils.js'
 
 class Node {
     constructor(value) {
-        this.value = value;
-        this.left = null;
-        this.right = null;
+        this.value = value
+        this.left = null
+        this.right = null
     }
 }
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+
+    isEmpty() {
+        return this.root === null
+    }
+
+    insert(value) {
+        const newNode = new Node(value)
+        if(this.isEmpty()) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+
+    insertNode(root, newNode) {
+        if(newNode.value < root.value) {
+            if(root.left === null) {
+                root.left = newNode
+            } else {
+                this.insertNode(root.left, newNode)
+            }
+        } else {
+            if(root.right === null) {
+                root.right = newNode
+            } else {
+                this.insertNode(root.right, newNode)
+            }
+        }
+    }
+
+    search(root, value) {
+        if(!root) {
+            return false
+        } else {
+            if(root.value === value) {
+                return true
+            } else if(value < root.value) {
+                return this.search(root.left, value)
+            } else {
+                return this.search(root.right, value)
+            }
+        }
+    }
+}
+
+const bst = new BinarySearchTree()
+
+/**
+ *      Inserts 3 nodes to generate
+ *      the next tree:
+ * 
+ *       10
+ *      /  \
+ *     5    15
+ * 
+ */ 
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
+
+// Check if those values exists on the tree
+const isValue10Present = bst.search(bst.root, 10) ? VALUE_PRESENT : VALUE_NOT_PRESENT
+const isValue5Present = bst.search(bst.root, 5) ? VALUE_PRESENT : VALUE_NOT_PRESENT
+const isValue15Present = bst.search(bst.root, 15) ? VALUE_PRESENT : VALUE_NOT_PRESENT
+const isValue20Present = bst.search(bst.root, 20) ? VALUE_PRESENT : VALUE_NOT_PRESENT
+
+console.log(`Computer executed a search for value 10 and returned saying "${isValue10Present}"`)
+console.log(`Computer executed a search for value 5 and returned saying "${isValue5Present}"`)
+console.log(`Computer executed a search for value 15 and returned saying "${isValue15Present}"`)
+console.log(`Computer executed a search for value 20 and returned saying "${isValue20Present}"`)
---
+++ utils.js
@@ -0,0 +1,2 @@
+export const VALUE_PRESENT = 'value is here'
+export const VALUE_NOT_PRESENT = 'i did not found the value'
